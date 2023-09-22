54cd62968d3026deda8b7e5666592d6edafa230e
Gastech99 <diabygassamad16@gmail.com>
2023-09-22 15:05:10 +0000

    Utilisation de useState et des événement

--- src/components/search.js
+++ src/components/search.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import apiClient from '../services/api'
 
 function Search() {
@@ -6,19 +6,31 @@ function Search() {
     // Ajout de l'API
     apiClient.get('?term=jack+jonhson')
     .then(response => console.log(response))
-  })
+  }, [])
+
+  const [name, setName] = useState("")
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name)
+  }
 
   return (
     <div className='container'>
         <p>Rechercher une musique</p>
+        <form onSubmit={handleSubmit}>
         <div className='row'>
             <div className='col-lg-4'>
-                <input type='text' className='form-control' placeholder="Entrer le nom d'un artiste"/>
+                <input type='text' name='name' value={name} 
+                className='form-control'
+                onChange={e=> setName(e.target.value)}
+                placeholder="Entrer le nom d'un artiste"/>
             </div>
             <div className='col-lg-4'>
                 <button className='btn btn-primary'>Rechercher</button>
             </div>
         </div>
+        </form>
     </div>
   )
 }
