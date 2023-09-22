5c76e83b18f0229e282eb22a007b401a432fe686
Gastech99 <diabygassamad16@gmail.com>
2023-09-22 12:49:23 +0000

    Importation et ajout de l'API avec Axios

--- src/components/search.js
+++ src/components/search.js
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import apiClient from '../services/api'
 
 function Search() {
+  useEffect(() => {
+    // Ajout de l'API
+    apiClient.get('?term=jack+jonhson')
+    .then(response => console.log(response))
+  })
+
   return (
     <div className='container'>
         <p>Rechercher une musique</p>
         <div className='row'>
             <div className='col-lg-4'>
-                <input type='text' placeholder="Entrer le nom d'un artiste"/>
+                <input type='text' className='form-control' placeholder="Entrer le nom d'un artiste"/>
             </div>
-            <div className='col-lg-3'>
+            <div className='col-lg-4'>
                 <button className='btn btn-primary'>Rechercher</button>
             </div>
         </div>
