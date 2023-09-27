e8bc828b20076c8ddd0c284feb4345fdc5d47c2b
Gastech99 <diabygassamad16@gmail.com>
2023-09-27 16:16:43 +0000

    Ajout et lecture des musiaues avec la bibliothèque ReactAudioPlayer

--- src/components/music.js
+++ src/components/music.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useState } from "react";
+import ReactAudioPlayer from "react-audio-player";
 
-function Music() {
+function Music({ music }) {
+
+  const [favoris, setFavoris] = useState([])
+
+  const addFavoris = (music) => {
+    console.log(music, "music AddFavoris")
+  }
+
   return (
-    <div>
-        <p>Liste des musiques</p>
-    </div>
-  )
+    <>
+      <div className="col-md-2">
+        <div className="card" style={{ width: "18rem" }}>
+          <img src={music.artworkUrl100} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item">{music.artistName}</li>
+              <li className="list-group-item">{music.trackCensoredName}</li>
+              <button className="btn btn-primary" onClick={() => addFavoris(music)}>Ajouter aux favoris</button>
+              {/**
+               * Ajout et lecture de la musique avec ReactAudioPlayer
+               */}
+              <ReactAudioPlayer src={music.previewUrl} controls />
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Music
+export default Music;
--- src/components/search.js
+++ src/components/search.js
@@ -1,38 +1,66 @@
-import React, { useEffect, useState } from 'react'
-import apiClient from '../services/api'
+import React, { useEffect, useState } from "react";
+import apiClient from "../services/api";
+import Music from "./music";
 
 function Search() {
   useEffect(() => {
     // Ajout de l'API
-    apiClient.get('?term=jack+jonhson')
-    .then(response => console.log(response))
-  }, [])
 
-  const [name, setName] = useState("")
+    console.log(list, "UseEffect")
+  }, []);
+
+  const [name, setName] = useState("");
+  const [list, setList] = useState([])
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(name)
-  }
+    console.log(name);
+
+    apiClient
+      .get("?term="+name+'&limit=25')
+      .then((response) => {
+        setList(response.data.results)
+
+        console.log(list, "Voir la liste")
+      });
+
+  };
 
   return (
-    <div className='container'>
+    <>
+      <div className="container mb-3">
         <p>Rechercher une musique</p>
         <form onSubmit={handleSubmit}>
-        <div className='row'>
-            <div className='col-lg-4'>
-                <input type='text' name='name' value={name} 
-                className='form-control'
-                onChange={e=> setName(e.target.value)}
-                placeholder="Entrer le nom d'un artiste"/>
+          <div className="row">
+            <div className="col-lg-4">
+              <input
+                type="text"
+                name="name"
+                value={name}
+                className="form-control"
+                onChange={(e) => setName(e.target.value)}
+                placeholder="Entrer le nom d'un artiste"
+              />
             </div>
-            <div className='col-lg-4'>
-                <button className='btn btn-primary'>Rechercher</button>
+            <div className="col-lg-4">
+              <button className="btn btn-primary">Rechercher</button>
             </div>
-        </div>
+          </div>
         </form>
-    </div>
-  )
+      </div>
+      {/**
+        Affichage de la liste des musiques en les passant en props
+       */}
+      <div className="container m-9">
+        <div className="row">
+          {
+            list.map((item, index) => <div className="col-md-4 mb-4" key={index}>
+              <Music music={item}/></div>)
+          }
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Search
+export default Search;
